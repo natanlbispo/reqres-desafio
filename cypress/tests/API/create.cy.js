2383d6babbea037c603512d435bc4c5b210da1be
natanlbispo <natan.bispo9@gmail.com>
2024-01-04 17:58:03 -0300

    Dace the night - WIP: add contract test with joi

--- cypress/tests/API/create.cy.js
+++ cypress/tests/API/create.cy.js
@@ -1,8 +1,17 @@
 const faker = require('faker-br');
+import createSchema from './contract/create.contracts';
 describe('CREATE API', () => {
-    it.only('Creating user', () => {
+    it('Creating user', () => {
         const name = faker.name.firstName();
-        const job = faker.name.department()
+        const job = faker.name.jobTitle();
         cy.createUser(name, job);
+    })
+
+    it.only('Create Contract', () => {
+        const name = faker.name.firstName();
+        const job = faker.name.jobTitle();
+        cy.createUser(name, job).then(response => {
+            return createSchema.validateAsync(response.body);
+        });
     })
 })
